11a0e8056aacfe21ea7bc6f30572d328c402bd79
Sujal gohel <128742497+Sujx1309@users.noreply.github.com>
2026-09-07 01:05:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(
+      `${import.meta.env.BASE_URL}sw.js`
+    );
+  });
+}
